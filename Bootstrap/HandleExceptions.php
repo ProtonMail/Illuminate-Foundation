9d9dd878460a75c33ed87c76a0addd05fa64abdb
Skyler Fenn <skylerfenn@users.noreply.github.com>
2019-07-18 15:00:36 -0600

    HandleExceptions: Add scope for static variable

--- Bootstrap/HandleExceptions.php
+++ Bootstrap/HandleExceptions.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Exception\FatalThrowableError;
 
 class HandleExceptions
 {
-    static $reservedMemory;
+    public static $reservedMemory;
 
     /**
      * Reserved memory so that errors can be displayed properly on memory exhaustion.
