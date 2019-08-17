b725ad5c590f91608df49189a4c06ee8f5a4531b
antonkomarev <ell@cybercog.su>
2019-08-18 00:00:27 +0300

    Add Artisan db:wipe command

--- Providers/ArtisanServiceProvider.php
+++ Providers/ArtisanServiceProvider.php
@@ -4,6 +4,7 @@ namespace Illuminate\Foundation\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Queue\Console\TableCommand;
+use Illuminate\Database\Console\WipeCommand;
 use Illuminate\Foundation\Console\UpCommand;
 use Illuminate\Foundation\Console\DownCommand;
 use Illuminate\Auth\Console\ClearResetsCommand;
