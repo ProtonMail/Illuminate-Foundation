4553e47ca23ae7fdc91624b3e780c47f58b47fa8
Taylor Otwell <taylor@laravel.com>
2019-08-20 10:45:17 -0500

    version

--- Application.php
+++ Application.php
@@ -36,7 +36,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '5.8.32';
+    const VERSION = '5.8.33';
 
     /**
      * The base path for the Laravel installation.
