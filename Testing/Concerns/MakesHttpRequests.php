5e1bdcbb0a6a97dd06a426f262ca6434359309bb
Jason McCreary <jason@pureconcepts.net>
2019-09-24 19:28:57 -0600

    Add methods for sending cookies with test requests

--- Testing/Concerns/MakesHttpRequests.php
+++ Testing/Concerns/MakesHttpRequests.php
@@ -12,6 +12,13 @@ use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 trait MakesHttpRequests
 {
     /**
+     * Additional cookies for the request.
+     *
+     * @var array
+     */
+    protected $defaultCookies = [];
+
+    /**
      * Additional headers for the request.
      *
      * @var array
@@ -148,7 +155,7 @@ trait MakesHttpRequests
     /**
      * Define additional cookies to be sent with the request.
      *
-     * @param  array  $cookies
+     * @param  array $cookies
      * @return $this
      */
     public function withCookies(array $cookies)
@@ -161,8 +168,8 @@ trait MakesHttpRequests
     /**
      * Add a cookie to be sent with the request.
      *
-     * @param  string  $name
-     * @param  string  $value
+     * @param  string $name
+     * @param  string $value
      * @return $this
      */
     public function withCookie(string $name, string $value)
